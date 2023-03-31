a3eb497f0de9ed357e90859900f23e538a0ad250
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-03-31 14:25:23 +0100

    some tweaks

--- link_rapidxml.sh
+++ link_rapidxml.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
+
 INCLUDE_DIR=/usr/include
-TARGET_DIR=include/rapidxml
+TARGET_DIR="$1"/include
 
 hfiles=($INCLUDE_DIR/rapidxml*.*)
 
+mkdir -p $TARGET_DIR
+
 if [ -d "$INCLUDE_DIR/rapidxml" ]; then
-    ln -s "$INCLUDE_DIR/rapidxml" $TARGET_DIR
+    ln -s "$INCLUDE_DIR/rapidxml/" $TARGET_DIR
 elif [ -e "${hfiles[0]}" ]; then
+    TARGET_DIR="$TARGET_DIR"/rapidxml
     rm -rf "$TARGET_DIR"
     mkdir -p "$TARGET_DIR"
     find $INCLUDE_DIR/rapidxml*.* | while read rapid_xml_path; do
